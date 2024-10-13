26a53ec224ad0a1bb0ad9631c5a6fbdbbf7c6397
TheAdduser <EasyRider190@gmail.com>
2024-10-13 14:07:40 +0200

    created main page

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../_components/Header";
+import Sidebar from "../_components/Sidebar";
+import KanbanBoard from "../_components/KanbanBoard";
+import Footer from "../_components/Footer";
+
+const Home: React.FC =(props) => {
+  return(
+    <div className="flex flex-col h-screen bg-gray-900">
+      <Header/>
+        
+         <div className="flex flex-grow">
+         <Sidebar />
+ 
+         
+         <KanbanBoard />
+       </div>
+ 
+       
+       <Footer />
+     </div>
+   );
+ };
+ 
+ export default Home;
