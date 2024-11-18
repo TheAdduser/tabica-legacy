866f0f8e7203e4a13353df61e8bc4b584008d074
TheAdduser <EasyRider190@gmail.com>
2024-11-18 16:01:47 +0100

    add turso integration

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 import { env } from "~/env";
+
+const tursoClient = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
+});
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter: PrismaLibSQL({ client: tursoClient }),
   });
 
 const globalForPrisma = globalThis as unknown as {
