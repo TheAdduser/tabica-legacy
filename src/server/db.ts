321362cbd3a69bf755d2a8848afd3b46102cf123
TheAdduser <EasyRider190@gmail.com>
2024-11-29 14:03:27 +0100

    prepare for

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,25 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
+import PocketBase from 'pocketbase';
+import { env } from '~/env';
 
-import { env } from "~/env";
-
-const tursoClient = createClient({
-  url: env.DATABASE_URL,
-  authToken: env.AUTH_TOKEN,
-});
-
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: PrismaLibSQL({ client: tursoClient }),
-  });
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+const pb = new PocketBase('http://localhost:8090');
